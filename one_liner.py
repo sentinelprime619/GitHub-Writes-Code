cf21e48b68f599838b3af66486af2bb246342127
sentinelprime619 <72153964+sentinelprime619@users.noreply.github.com>
2020-09-30 23:45:06 +0530

    Adding string reversal one-liner (#2)

--- one_liner.py
+++ one_liner.py
@@ -1,1 +1,5 @@
 print([(i, j) for i in range(3) for j in range(7)])
+
+#String reversal
+str = "original"
+print("Reverse of {} is {}".format(str,str[::-1])) #Output : Reverse of original is lanigiro
